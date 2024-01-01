d09ee26e18611311984e6be6bdbd0341a2792439
Milo-91 <68451766+Milo-91@users.noreply.github.com>
2024-01-01 21:40:09 +0800

    add a file to download file from huggingface

---
+++ huggingface_download_model.py
@@ -0,0 +1,9 @@
+from huggingface_hub import hf_hub_download, login
+
+login(token = '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')
+
+hf_hub_download(
+    repo_id = 'TheBloke/vicuna-7B-v1.5-GGUF',
+    filename = 'vicuna-7b-v1.5.Q8_0.gguf',
+    local_dir = 'model'
+)
