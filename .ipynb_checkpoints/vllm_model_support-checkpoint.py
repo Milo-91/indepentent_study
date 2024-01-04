c806b12bd4f166d4a4558f74b3fe240f56718609
Milo-91 <milolin9112@gmail.com>
2024-01-04 19:52:38 +0000

    try rellm method using wizardmath-7b

--- .ipynb_checkpoints/vllm_model_support-checkpoint.py
+++ .ipynb_checkpoints/vllm_model_support-checkpoint.py
@@ -1,5 +1,7 @@
 from vllm import LLM
 
-llm = LLM(model = 'WizardLM/WizardMath-13B-V1.0')  # Name or path of your model
-output = llm.generate("Hello, my name is")
+llm = LLM(model = 'WizardLM/WizardMath-7B-V1.1')  # Name or path of your model
+prompt_template = '"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:"
+'
+output = llm.generate(prompt_template.format(instruction = "Hello, my name is"))
 print(output)
