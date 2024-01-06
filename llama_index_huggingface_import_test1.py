3bfd04308f3640b2d79e07882b9a3ff69f0b41d1
Milo-91 <milolin9112@gmail.com>
2024-01-06 13:04:00 +0000

    add do sample parameters (need when temperature is on)

--- llama_index_huggingface_import_test1.py
+++ llama_index_huggingface_import_test1.py
@@ -8,20 +8,21 @@ from llama_index.prompts.lmformatenforcer_utils import (
 )
 
 llm = HuggingFaceLLM(
-    model_name = "EleutherAI/llemma_7b",
-    max_new_tokens = 1024,
-    model_kwargs={"n_gpu_layers": -1},
-    generate_kwargs={"temperature": 0.7},
+    model_name = "llemma_7b",
+    max_new_tokens = 2048,
+    model_kwargs={},
+    generate_kwargs={"temperature": 0.7, "do_sample": True},
 )
 
 regex = r'"Hello, my name is (?P<name>[a-zA-Z]*)\. I was born in (?P<hometown>[a-zA-Z]*). Nice to meet you!"'
-
+'''
 regex_parser = lmformatenforcer.RegexParser(regex)
 lm_format_enforcer_fn = build_lm_format_enforcer_function(llm, regex_parser)
 with activate_lm_format_enforcer(llm, lm_format_enforcer_fn):
     output = llm.complete(
         "Here is a way to present myself, if my name was John and I born in Boston: "
     )
-
+'''
+output = llm.complete("1+2=?")
 print(output)
-print(re.match(regex, output.text).groupdict())
+# print(re.match(regex, output.text).groupdict())
