fb23df645febe488a48b9b160b7e2911221c165b
Milo-91 <milolin9112@gmail.com>
2024-01-07 13:41:05 +0000

    vicuna-13B.Q8.0 test 3 sets

--- .ipynb_checkpoints/huggingface_download_model-checkpoint.py
+++ .ipynb_checkpoints/huggingface_download_model-checkpoint.py
@@ -3,8 +3,8 @@ from huggingface_hub import hf_hub_download, login
 login(token = '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')
 
 hf_hub_download(
-    repo_id = 'TheBloke/vicuna-13B-v1.5-16K-GGUF',
-    filename = 'vicuna-13b-v1.5-16k.Q8_0.gguf',
+    repo_id = 'TheBloke/vicuna-13B-v1.5-GGUF',
+    filename = 'vicuna-13b-v1.5.Q8_0.gguf',
     local_dir = 'models'
 )
 
