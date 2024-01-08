a9619ac5245fd35107897e9fd05242edda181f7f
Milo-91 <68451766+Milo-91@users.noreply.github.com>
2024-01-08 12:44:03 +0800

    generator left numbers use program to build instead to llm response

--- game24/game24.py
+++ game24/game24.py
@@ -54,28 +54,28 @@ don't forget to convert the numbers in the input to the result number in possibl
 and then put new numbers behind "left: " in possible next steps
 Input: 2 8 8 14
 8 Possible next steps:
-2 + 8 = 10 (left: 8 10 14)
-8 / 2 = 4 (left: 4 8 14)
-14 + 2 = 16 (left: 8 8 16)
-2 * 8 = 16 (left: 8 14 16)
-8 - 2 = 6 (left: 6 8 14)
-14 - 8 = 6 (left: 2 6 8)
-14 /  2 = 7 (left: 7 8 8)
-14 - 2 = 12 (left: 8 8 12)
+2 + 8 = 10
+8 / 2 = 4
+14 + 2 = 16
+2 * 8 = 16
+8 - 2 = 6
+14 - 8 = 6
+14 /  2 = 7
+14 - 2 = 12
 
 Input: 2 8 14
 5 Possible next steps:
-2 + 8 = 10 (left: 10 14)
-14 - 8 = 6 (left: 2 6)
-14 + 2 = 16 (left: 8 16)
-8 / 2 = 4 (left: 4 14)
-2 - 8 = -6 (left: -6 14)
+2 + 8 = 10
+14 - 8 = 6
+14 + 2 = 16
+8 / 2 = 4
+2 - 8 = -6
 
 Input: 10 14
 3 Possible next steps:
-10 + 14 = 24 (left: 24)
-10 - 14 = -4 (left: -4)
-10 * 14 = 140 (left: 140)
+10 + 14 = 24
+10 - 14 = -4
+10 * 14 = 140
 
 Input: {input}
 {k} Possible next steps:
